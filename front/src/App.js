b80a2668bf1c327b2590dca996a20344442d5e00
Ballbot <5252bb@daum.net>
2023-06-26 12:50:22 +0000

    작업중

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 function App() {
+  const { data, error, isLoading } = useSWR('https://day-api.blbt.app/v1/business-day', fetcher, {
+    refreshInterval: 3600000,
+  });
+
+  console.log(data, error, isLoading)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="border">
+        <div id="selected-user">
+          <h3><span id="nickname">nickname</span> 님 앞으로</h3>
+          <h1><span id="business-day-num">0</span> 영업일</h1>
+        </div>
+        <div id="select-user" className="container">
+          <h4>내 영업일을 찾거나 등록</h4>
+          <div class="input-group">
+            <span class="input-group-text">닉네임</span>
+            <input type="text" aria-label="닉네임" class="form-control" placeholder="이름 또는 닉네임"/>
+            <span class="input-group-text">퇴사일</span>
+            <input type="date" aria-label="퇴사일" class="form-control" />
+            <button class="btn btn-success" type="button" style={{ width: 100 }}>등록</button>
+          </div>
+          <br />
+
+          <p style={{ color: 'rgb(134, 134, 134)' }}>표를 클릭하면 상단에 표시됩니다.</p>
+          {
+            !isLoading && (
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">닉네임</th>
+                  <th scope="col">퇴사일</th>
+                  <th scope="col">등록일</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data.map((d) => (
+                  <tr>
+                    <td class="table-dark">{d.nickname}</td>
+                    <td class="table-dark">{d.leaveAt} ({d.businessDayCount}일 남음)</td>
+                    <td class="table-dark">{d.createdAt}</td>
+                  </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+            )
+          }
+          
+        </div>
+      </div>
     </div>
   );
 }
--- src/routers/v1.router.ts
+++ src/routers/v1.router.ts
@@ -1,9 +1,12 @@
-import { Router, withParams } from 'itty-router';
+import { Router } from 'itty-router';
+import { createCors } from 'itty-cors'
 import Container from 'typedi';
 import { BusinessDayController } from '../components/business-day/business-day.controller';
 
 const v1Router = Router();
 const controllers = [Container.get(BusinessDayController)];
-v1Router.all('*', withParams);
+const { preflight } = createCors();
+
+v1Router.all('*', preflight);
 controllers.forEach((controller) => controller.bind(v1Router, '/v1'));
 export default v1Router;
